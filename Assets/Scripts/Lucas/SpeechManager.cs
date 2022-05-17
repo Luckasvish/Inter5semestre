4d6868d65dadd5730e32b3a0c0e6a479642911e0
Lucas Ferreira Lima da Cunha <luckasvish@gmail.com>
2022-05-17 00:08:18 -0300

    Mudança de nome nas cenas

---
+++ Assets/Scripts/Lucas/SpeechManager.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ENFIA O NOME NO CU", menuName = "ENFIA O CAMINHO NO CU DE NOVO")]
+public class SpeechManager : ScriptableObject
+{
+    public string[] clientSpeech;
+
+}
