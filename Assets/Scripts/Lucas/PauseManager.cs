4613acf326e9b14c8138c4a5787b866d70597248
Lucas Ferreira Lima da Cunha <luckasvish@gmail.com>
2022-06-02 01:56:22 -0300

    Pause e correção UI

--- Assets/Scripts/Lucas/PauseManager.cs
+++ Assets/Scripts/Lucas/PauseManager.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 
 public class PauseManager : MonoBehaviour
 {
+    [SerializeField]
+    GameObject PauseUI;
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause(true);
+        }
+#if UNITY_EDITOR
+        if(Input.GetKeyDown(KeyCode.P))
+        {
+            Bank.instance.ChangeMoneyAmount(20, true);
+        }
+        if (Input.GetKeyDown(KeyCode.Mouse4))
+        {
+            SceneManage.GoToNextScene();
+        }
+#endif
+    }
     public void Pause(bool can_pause)
     {
-        if(can_pause)
+        if (can_pause)
             Time.timeScale = 0;
         if (!can_pause)
             Time.timeScale = 1;
+        PauseUI.SetActive(can_pause);
     }
 }
